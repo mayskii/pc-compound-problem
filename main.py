7b2bc11dac74e5a0da62f7808b77b14b015ec4dd
Maya Zakharova <mayzakharova@gmail.com>
2026-05-06 18:53:24 -0500

    done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 def segmentable(s, vocab_list):
-    pass
+    vocab = set(vocab_list)
+    n = len(s)
+    
+    dp = [False] * (n + 1)
+    dp[0] = True
+    
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in vocab:
+                dp[i] = True
+                break
+    
+    return dp[n]
 
 
 s = "softwareengineer"
